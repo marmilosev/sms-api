bb76a448bb3ed48de568ea6266627fc09f03ba64
marija <mmilosevic1411@gmail.com>
2023-04-20 16:48:22 +0200

    Only deleteUser and getUserById working fine.

--- demo4/src/main/java/com/infobip/demo4/controller/UserController.java
+++ demo4/src/main/java/com/infobip/demo4/controller/UserController.java
@@ -27,68 +27,16 @@ public class UserController {
     private ModelMapper modelMapper;
     private ApiResponse apiResponse;
 
-//    @PostMapping("/add")
-//    public ResponseEntity<ApiResponse> createUser(@Valid @RequestBody User user, Errors errors){
-//        apiResponse = new ApiResponse();
-//        if(errors.hasErrors()){
-//            if(user.getFirstName().isBlank()) {
-//                apiResponse.setCode(1);
-//                apiResponse.setMessage("Error with first name input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/1");
-//            }
-//            else if(user.getLastName().isBlank()) {
-//                apiResponse.setCode(2);
-//                apiResponse.setMessage("Error with last name input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/2");
-//            }
-//            else if(user.getUsername().isBlank()) {
-//                apiResponse.setCode(3);
-//                apiResponse.setMessage("Error with username input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/3");
-//            }
-//            else if(user.getNumber().isBlank()) {
-//                apiResponse.setCode(4);
-//                apiResponse.setMessage("Error with number input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/4");
-//            }
-//            else if(user.getMail().isBlank()) {
-//                apiResponse.setCode(5);
-//                apiResponse.setMessage("Error with mail input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/5");
-//            }
-//            else if(user.getPassword().isBlank()) {
-//                apiResponse.setCode(6);
-//                apiResponse.setMessage("Error with password input");
-//                apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/6");
-//            }
-//
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
-//        }
-//
-//        // all ok - save User in database
-//        user=userService.saveUser(user);
-//
-//        apiResponse.setCode(7);
-//        apiResponse.setMessage("User with username " + user.getUsername() + " successfully created.");
-//        apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/7");
-//        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
-//    }
-
     @PostMapping("/add")
-    public ResponseEntity<?> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDto userDto) {
         Map<String, Object> jsonResponseMap = new LinkedHashMap<String, Object>();
         User user = modelMapper.map(userDto, User.class);
         userService.saveUser(user);
-        jsonResponseMap.put("status", 0);
+        jsonResponseMap.put("status", 1);
         jsonResponseMap.put("message", "User saved successfully");
         return new ResponseEntity<>(jsonResponseMap, HttpStatus.CREATED);
     }
 
-
-//    @GetMapping
-//    public List<User> getAllUsers(){
-//        return userService.getAllUsers();
-//    }
     @GetMapping
     public ResponseEntity<?> getAllUsers(){
         Map<String, Object> jsonResponseMap = new LinkedHashMap<String, Object>();
@@ -109,16 +57,6 @@ public class UserController {
         }
 }
 
-
-//    @GetMapping("/{id}")
-//    public ResponseEntity<User> getUserById(@PathVariable("id") int id){
-//        User user = userService.getUserById(id);
-//        if(user != null) {
-//            return ResponseEntity.ok(user);
-//        } else{
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
         @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable("id") int id){
             Map <String, Object > jsonResponseMap = new LinkedHashMap<String, Object>();
@@ -136,28 +74,8 @@ public class UserController {
             }
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<ApiResponse> updateUser(@Valid @PathVariable("id") int id, @RequestBody User user, Errors errors){
-//        apiResponse = new ApiResponse();
-//
-//        user.setIdUser(id);
-//        try {
-//            userService.updateUser(user);
-//            apiResponse.setCode(8);
-//            apiResponse.setMessage("User with username " + user.getUsername() + " has been successfully updated.");
-//            apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/8");
-//            return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
-//        }catch (Exception e){
-//            apiResponse.setCode(9);
-//            apiResponse.setMessage("Error updating customer");
-//            apiResponse.setDocsURL("https://mmilosevic-diplomski-api.com/users/v1/9");
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
-//        }
-//
-//    }
-
     @PutMapping("/{id}")
-    public ResponseEntity < ? > updateUser(@PathVariable Integer id, @RequestBody UserDto userDto) {
+    public ResponseEntity < ? > updateUser(@Valid @PathVariable int id, @RequestBody UserDto userDto) {
         Map < String, Object > jsonResponseMap = new LinkedHashMap < String, Object > ();
         try {
             User user = userService.getUserById(id);
@@ -179,13 +97,8 @@ public class UserController {
         }
     }
 
-
-//    @DeleteMapping("/{id}")
-//    public String deleteUser(@PathVariable("id") int id){
-//        return userService.deleteUser(id);
-//    }
     @DeleteMapping("/{id}")
-    public ResponseEntity < ? > deleteUser(@PathVariable Integer id) {
+    public ResponseEntity < ? > deleteUser(@PathVariable int id) {
         Map < String, Object > jsonResponseMap = new LinkedHashMap < String, Object > ();
         try {
             User user = userService.getUserById(id);
