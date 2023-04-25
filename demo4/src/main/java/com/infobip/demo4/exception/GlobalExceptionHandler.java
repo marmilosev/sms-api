128afcd5da3899ae7235814363be1eb6fe475de8
marija <mmilosevic1411@gmail.com>
2023-04-25 20:45:12 +0200

    Adding .gitignore

--- demo4/src/main/java/com/infobip/demo4/exception/GlobalExceptionHandler.java
+++ demo4/src/main/java/com/infobip/demo4/exception/GlobalExceptionHandler.java
@@ -51,27 +51,27 @@ public class GlobalExceptionHandler {
 //        return new ResponseEntity<>(apiResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 //    }
 
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<Map<String, List<String>>> handleNotFoundException(UsernameNotFoundException ex) {
-        List<String> errors = Collections.singletonList(ex.getMessage());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Map<String, List<String>>> handleGeneralExceptions(Exception ex) {
-        List<String> errors = Collections.singletonList(ex.getMessage());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public final ResponseEntity<Map<String, List<String>>> handleRuntimeExceptions(RuntimeException ex) {
-        List<String> errors = Collections.singletonList(ex.getMessage());
-        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private Map<String, List<String>> getErrorsMap(List<String> errors) {
-        Map<String, List<String>> errorResponse = new HashMap<>();
-        errorResponse.put("errors", errors);
-        return errorResponse;
-    }
+//    @ExceptionHandler(UsernameNotFoundException.class)
+//    public ResponseEntity<Map<String, List<String>>> handleNotFoundException(UsernameNotFoundException ex) {
+//        List<String> errors = Collections.singletonList(ex.getMessage());
+//        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(Exception.class)
+//    public final ResponseEntity<Map<String, List<String>>> handleGeneralExceptions(Exception ex) {
+//        List<String> errors = Collections.singletonList(ex.getMessage());
+//        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//
+//    @ExceptionHandler(RuntimeException.class)
+//    public final ResponseEntity<Map<String, List<String>>> handleRuntimeExceptions(RuntimeException ex) {
+//        List<String> errors = Collections.singletonList(ex.getMessage());
+//        return new ResponseEntity<>(getErrorsMap(errors), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//
+//    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+//        Map<String, List<String>> errorResponse = new HashMap<>();
+//        errorResponse.put("errors", errors);
+//        return errorResponse;
+//    }
 }
