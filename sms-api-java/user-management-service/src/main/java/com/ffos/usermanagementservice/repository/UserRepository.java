99bd1db5c714b70d8802994ced49e681693e2180
marija <mmilosevic1411@gmail.com>
2023-07-05 19:10:32 +0200

    Repsitory created

--- sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/repository/UserRepository.java
+++ sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.ffos.usermanagementservice.repository;
 
-public interface UserRepository {
+import com.ffos.usermanagementservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from User u where u.username=:username")
+    User findByUsername(@Param("username") String username);
 }
