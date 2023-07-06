74c5d71c31e97fc430ef9b796534df6b2a997934
marija <mmilosevic1411@gmail.com>
2023-07-06 15:34:12 +0200

    Creating new microservice for sending messages

---
+++ sms-api-java/send-message-service/src/main/java/com/ffos/sendmessageservice/SendMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ffos.sendmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMessageServiceApplication.class, args);
+	}
+
+}
