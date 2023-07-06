414306efbb058b25237236d25a803bac882afbdd
marija <mmilosevic1411@gmail.com>
2023-07-06 09:09:43 +0200

    Service created and files for catching exceptions.

--- sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/model/User.java
+++ sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/model/User.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Builder
 @Getter
 @Setter
 public class User {
--- sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/repository/UserRepository.java
+++ sms-api-java/user-management-service/src/main/java/com/ffos/usermanagementservice/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.username=:username")
     User findByUsername(@Param("username") String username);
 }
