94a82db29b3d5f2969b947abd1b51d34e98b3369
marija <mmilosevic1411@gmail.com>
2023-07-10 22:33:32 +0200

    Created new microservice for sending messages

---
+++ sms-api-kotlin/sendmessageservice/src/main/kotlin/com/example/sendmessageservice/SendmessageserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sendmessageservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SendmessageserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SendmessageserviceApplication>(*args)
+}
---
+++ sms-api-kotlin/sendmessageservice/src/test/kotlin/com/example/sendmessageservice/SendmessageserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sendmessageservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SendmessageserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
