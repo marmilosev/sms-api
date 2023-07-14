d1c116c51c51178fba40669cae71b3db3b54f5ca
marija <mmilosevic1411@gmail.com>
2023-07-14 09:46:49 +0200

    Other microservices created

---
+++ sms-api-kotlin/discoveryservice/src/main/kotlin/com/example/discoveryservice/DiscoveryserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.discoveryservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiscoveryserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiscoveryserviceApplication>(*args)
+}
