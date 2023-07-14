13be68ede6421e92899a408bdd602d104819f57e
marija <mmilosevic1411@gmail.com>
2023-07-14 09:51:16 +0200

    sendmessage and user service updated for microservices

--- sms-api-kotlin/userservice/src/main/kotlin/com/example/userservice/UserserviceApplication.kt
+++ sms-api-kotlin/userservice/src/main/kotlin/com/example/userservice/UserserviceApplication.kt
@@ -2,8 +2,10 @@ package com.example.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class UserserviceApplication
 
 fun main(args: Array<String>) {
